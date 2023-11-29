4b1e6556ad77290faaca77a5b506a560a03c0a8d
Emmanuel <ememeemmanuel3@gmail.com>
2023-11-29 14:04:31 +0000

    Updated version

--- index.js
+++ index.js
@@ -28,13 +28,9 @@ highlightInputArea()
 
 let results = {};
 
+shuffleColor()
 
-assignColors();
-
-for (let button of buttons) {
-    const color = items[button.id].color;
-    setButtonColor(button, color);
-}
+updateButtonColors()
 
 function eliminateItems(color) {
     const filtered = {};
@@ -44,46 +40,77 @@ function eliminateItems(color) {
             filtered[key] = items[key];
         }
     }
-    
-    assignColors();
 
     if (Object.keys(results).length === 0) {
         results = { ...filtered };
 
-    } else if(Object.keys(results).length > 1){
+    } else if (Object.keys(results).length > 1) {
         for (let key in results) {
             if (!filtered[key]) {
                 delete results[key];
             }
         }
-    }else{
-        const finalKey = Object.keys(results)[0];
-        if(index < outputs.length){
+
+        if (index < outputs.length && Object.keys(results).length === 1) {
+            const finalKey = Object.keys(results)[0];
             outputs[index].innerText = finalKey[0]
             index += 1
             highlightInputArea()
             results = {}
-        }else{
-            refresh()
         }
-        
+    } else {
+        refresh()
     }
-    updateButtonColors();
+
+    shuffleColor()
+    updateButtonColors()
+
 }
 
-function assignColors() {
-    const colors = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
-    // Fisher-Yates shuffle algorithm
-    for (let i = colors.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [colors[i], colors[j]] = [colors[j], colors[i]];
+
+function shuffleColor() {
+    if (Object.keys(results).length > 1) {
+        const colors = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+        const resultColors = colors.slice(0, Object.keys(results).length)
+        for (let i = resultColors.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [resultColors[i], resultColors[j]] = [resultColors[j], resultColors[i]];
+        }
+
+        let i = 0
+        for (let key in results) {
+            results[key].color = resultColors[i]
+            i++
+        }
+
+        const remainingItems = colors.slice(Object.keys(results).length)
+        for (let i = remainingItems.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [remainingItems[i], remainingItems[j]] = [remainingItems[j], remainingItems[i]];
+        }
+        
+        let j = 0
+        for (let key in items) {
+            if (!results[key]) {
+                items[key].color = remainingItems[j]
+                j++
+            }
+        }
+
+    } else {
+        const colors = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+        for (let i = colors.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [colors[i], colors[j]] = [colors[j], colors[i]];
+        }
+
+        let i = 0
+        for (let key in items) {
+            items[key].color = colors[i]
+            i++
+        }
     }
 
-    // Assign shuffled colors to items
-    for (let i = 0; i < buttons.length; i++) {
-        const buttonId = buttons[i].id;
-        items[buttonId].color = colors[i];
-    }
 }
 
 function updateButtonColors() {
@@ -99,7 +126,14 @@ function setButtonColor(button, color) {
     button.style.backgroundColor = color === 0 ? yellow : ash;
 }
 
-function highlightInputArea(){
-    outputs[index].style.border = "5px solid black"
+function highlightInputArea() {
+    if (index < outputs.length) {
+        outputs[index].style.border = "5px solid black"
+    }
+
+}
+
+function refresh() {
+    window.location.reload();
 }
 
---
+++ utils.js
@@ -0,0 +1,5 @@
+ // Fisher-Yates shuffle algorithm
+    // for (let i = colors.length - 1; i > 0; i--) {
+    //     const j = Math.floor(Math.random() * (i + 1));
+    //     [colors[i], colors[j]] = [colors[j], colors[i]];
+    // } 
